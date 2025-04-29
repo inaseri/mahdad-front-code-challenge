de4074bd635f7b94123af11d34efe67013ea22a0
Iman Naseri <inaseri.20@gmail.com>
2025-04-29 18:38:24 +0330

    feat: Fix layout of Products & Checkout page

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -1,15 +1,15 @@
 <template>
   <h3 class="text-h3 color-danger-500">Checkout Now!</h3>
   <v-divider class="border-opacity-100 mt-2 mb-5" color="success"></v-divider>
-  <div class="flex">
-    <div class="flex-column	" v-for="item of cartStore.getUserCart()" :key="item.id">
+  <v-row>
+    <v-col v-for="item of cartStore.getUserCart()" :key="item.id" :md="4" :sm="12">
       <v-card class="mt-2" :title="item.name" :subtitle="item.category">
         <v-card-actions>
           <v-btn color="error" @click="removeProduct(item)">Remove From Card</v-btn>
         </v-card-actions>
       </v-card>
-    </div>
-  </div>
+    </v-col>
+  </v-row>
 </template>
 
 <script setup lang="ts">
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="flex">
-    <h3 class="text-h3 color-danger-500">Shop Now!</h3>
-    <v-divider class="border-opacity-100 mt-2 mb-5" color="success"></v-divider>
-    <div class="flex-column	" v-for="item of items" :key="item.id">
+  <h3 class="text-h3 color-danger-500">Shop Now!</h3>
+  <v-divider class="border-opacity-100 mt-2 mb-5" color="success"></v-divider>
+  <v-row>
+    <v-col v-for="item of items" :key="item.id" :md="4" :sm="12">
       <v-card class="mt-2" :title="item.name" :subtitle="item.category">
         <v-card-actions>
           <v-btn color="success" @click="addProduct(item)">Add To Card</v-btn>
         </v-card-actions>
       </v-card>
-    </div>
-  </div>
+    </v-col>
+  </v-row>
 </template>
 
 <script setup lang="ts">
